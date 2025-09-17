989ec60e2b1850a1fc8e2465b811e16b36a15e4d
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-17 19:23:22 +0100

    feat(navbar): Move the navbar for common path

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React, { useState} from "react";
-import { userCart } from "../context/CartContext";
-
-const Navbar = () => {
-    const [click, setClick] = useState(false);
-    const { cart } = userCart();
-
-    const handleClick = () => setClick(!click);
-}

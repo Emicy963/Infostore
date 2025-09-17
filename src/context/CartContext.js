30983bb67bd9b8459a478f13d570aab5f4285c6e
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-17 12:25:42 +0100

    feat(cart-context): Create function for generate cart code if don't generate on API

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -6,4 +6,10 @@ const CartContext = createContext();
 export const CartProvider = ({ childen }) => {
     const [cart, setCart] = useState(null);
     const [cartCode, setCartCode] = useState(localStorage.getItem("cart_code") || generateCartCode());
+
+    function generateCartCode() {
+        const code = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        localStorage.setItem("cart_code", code);
+        return code;
+    }
 }

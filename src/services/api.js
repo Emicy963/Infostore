71cfdfe4de3e2bcaa4825cf58657839570666a33
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-24 08:42:20 +0100

    feat(auth): Update de api service for taking authentication from API

--- src/services/api.js
+++ src/services/api.js
@@ -5,11 +5,48 @@ const api = axios.create({
 })
 
 api.interceptors.request.use(config => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("acessToken");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-})
+});
+
+// Interceptor for take with expired tokens
+api.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+
+        // If erro be 401 and yet don't have refresh token
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const refreshToken = localStorage.getItem("refreshToken");
+                if (refreshToken) {
+                    const response = await axios.post("http://localhost:8000/api/token/refresh/", {
+                        refresh: refreshToken
+                    });
+
+                    const { access } = response.data;
+                    localStorage.setItem("acessToken", access);
+
+                    // Update original request header with the new token
+                    originalRequest.headers.Authorization = `Bearer ${access}`
+                    return api(originalRequest);
+                }
+            } catch (refreshError) {
+                // If can't refresh token, logout the user
+                localStorage.removeItem("acessToken");
+                localStorage.removeItem("refreshToken");
+                window.location.href = "/login";
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default api;

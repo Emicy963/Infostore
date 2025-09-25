e7db85f2469d54462507a9023daf7ef0b1293ad7
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-09-25 19:35:46 +0100

    feat(register): Set the register context

---
+++ src/components/auth/Register.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [formError, setFormError] = useState("");
+    const { register, error, loading } = useAuth();
+    const navigate = useNavigate();
+
+    
+};
+
+export default Register;
